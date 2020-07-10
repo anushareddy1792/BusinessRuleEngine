ecabc632bfaaecf4a9bd7f2d791502e00918d528
Anusha Reddy <anusha.nagarathnamreddy@jda.com>
2020-07-10 09:08:47 +0530

    Created a interface for Interface for modifying packing slip

---
+++ BusinessRuleService/Interfaces/IPackingSlipAction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessRuleService.Interfaces
+{
+    /// <summary>
+    /// Interface for modifying packing slip
+    /// </summary>
+    interface IPackingSlipAction
+    {
+        bool AddFreeItemToSlip();
+    }
+}
