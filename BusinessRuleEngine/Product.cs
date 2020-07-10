599277f2513577767d5ec2beb30ace7e0ed2c31c
Anusha Reddy <anusha.nagarathnamreddy@jda.com>
2020-07-10 09:28:01 +0530

    Creating data for exceuting the rules.

---
+++ BusinessRuleEngine/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace BusinessRuleEngine
+{
+    internal class Product
+    {
+        public Product()
+        {
+        }
+
+        public object Type { get; set; }
+        public int Cost { get; set; }
+        public string Name { get; set; }
+        public int Id { get; set; }
+    }
+}
--- BusinessRuleEngine/Program.cs
+++ BusinessRuleEngine/Program.cs
@@ -1,12 +1,70 @@
-﻿using System;
+﻿using BusinessRuleService.Models.Enums;
+using System;
+using System.Collections.Generic;
 
 namespace BusinessRuleEngine
 {
     class Program
     {
+       
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ExecuteBussinessRule();
+        }
+
+        public static void ExecuteBussinessRule()
+        {
+            var products = GetProducts();
+            
+            Console.ReadLine();
+        }
+
+        private static List<Product> GetProducts()
+        {
+            var products = new List<Product>() {
+            new Product()
+            {
+                Type = ProductType.PhysicalProduct,
+                Cost = 500,
+                Name = "Mug",
+                Id = 1
+            },
+            new Product()
+            {
+                Type = ProductType.Book,
+                Cost = 1000,
+                Name = "Theory of Everything",
+                Id = 2
+            },
+             new Product()
+            {
+                Type = ProductType.MembershipActivate,
+                Cost = 2000,
+                Id = 3
+            },
+              new Product()
+            {
+                Type = ProductType.MembershipUpgrade,
+                Cost = 1000,
+                Id = 4
+            },
+              new Product()
+            {
+                Type = ProductType.Video,
+                Name = "Learning to Ski",
+                Cost = 500,
+                Id = 5
+            },
+                new Product()
+            {
+                Type = ProductType.Video,
+                Name = "Learning to Dance",
+                Cost = 200,
+                Id = 6
+            }
+        };
+
+            return products;
         }
     }
 }
